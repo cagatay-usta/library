74f488d4ecb770d8c4f6d4ec3d655c4f580c8311
cagatay-usta <cagatay_usta@windowslive.com>
2023-04-21 23:41:26 +0300

    complete add/remove functions

--- library/index.js
+++ library/index.js
@@ -1,6 +1,3 @@
-// create object constructor
-// create form and connect to constructor
-// connect delete button to also delete from memory
 
 const books = [
   {
@@ -8,7 +5,7 @@ const books = [
     author: "Sedat Kapanoglu",
     pages: "314",
     read: "Not read",
-    id: "1",
+    id: 0,
   },
   {
     title:
@@ -16,30 +13,34 @@ const books = [
     author: "Charles Petzold",
     pages: "393",
     read: "Read",
-    id: "2",
+    id: 1,
   },
   {
     title: "Deep Work",
     author: "Cal Newport",
     pages: "304",
     read: "Read",
-    id: "3",
+    id: 2,
   },
   {
     title: "Clean Code",
     author: "Robert Cecil Martin",
     pages: "464",
     read: "Not read",
-    id: "4",
-  },
-  {
-    title: "Pragmatic Programmer",
-    author: "Andy Hunt and Dave Thomas",
-    pages: "320",
-    read: "Read",
-    id: "5",
+    id: 3,
   },
 ];
+
+class Book {
+  constructor(title, author, pages, read) {
+      this.title = title,
+      this.author = author,
+      this.pages = pages,
+      this.read = read,
+      this.id = (books.length) ? +(books[books.length-1].id) + 1 : 0; // if all books are deleted, set the id to 0, else set it +1 of the last book
+      books.push(this);
+  }
+}
 
 function createHtml(html) {
   const template = document.createElement("template");
@@ -66,7 +67,7 @@ function createCard(book) {
         </div>
 
         <div class="my-4 flex gap-4 justify-around">
-          <span class="material-symbols-rounded big text-neutral">
+          <span class="material-symbols-rounded big text-neutral readButton">
             check_circle
           </span>
           <span class="material-symbols-rounded big text-brand delete">
@@ -76,8 +77,40 @@ function createCard(book) {
       </div>
 
 `);
-
   return newCard;
+}
+
+
+const addButton = document.querySelector("#add");
+const readButton = document.querySelector('#read');
+const formCard = document.querySelector('.form-card');
+
+let readState = "Not read";
+
+function addBook() {
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const read = readState;
+
+  if (title.value && author.value && pages.value) {
+    const newBook = new Book(title.value, author.value, pages.value, read);
+    document.getElementById("main").appendChild(createCard(newBook));
+    
+    // clean form and state
+    readState = "Not read";
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+
+    readButton.classList.remove("clicked");
+    formCard.classList.toggle("closed");
+    addButton.classList.toggle("clicked");
+  }
+}
+
+function deleteBook() {
+
 }
 
 function populateScreen(books) {
@@ -86,25 +119,52 @@ function populateScreen(books) {
   });
 }
 
+
 populateScreen(books);
 
-const addButton = document.querySelector("#add");
-const readButton = document.querySelector('#read');
-const formCard = document.querySelector('.form-card');
-
-
-// listens document for event delegation
+// listens the document for event delegation
 document.addEventListener("click", (e) => {
+  
   e.preventDefault();
+
   if (e.target.matches(".delete")) {
-    e.target.parentNode.parentNode.remove();
-  }
-  if (e.target.matches("#add")) {
+    const selectedCard = e.target.parentNode.parentNode;
+    // find the index of the book and delete it from the array
+    const filteredBook = books.filter(book => {
+      return book.id === +(selectedCard.dataset.number);
+    });
+    const bookIndex = books.indexOf(filteredBook[0]);
+    if (bookIndex != -1) {
+      books.splice(bookIndex, 1);
+    }
+
+    selectedCard.remove();
+
+
+  } else if (e.target.matches("#add")) {
     addButton.classList.toggle("clicked");
     formCard.classList.toggle("closed");
-    document.getElementById("main").appendChild(createCard(books[0]));
-  }
-  if (e.target.matches("#read")) {
+    
+  } else if (e.target.matches("#read")) {
     readButton.classList.toggle("clicked");
+    readState = (readState === "Not read") ? "Read" : "Not read";
+
+  } else if (e.target.matches("#addLibrary")) {
+    addBook();
+
+  } else if (e.target.matches(".readButton")) {
+    const selectedCard = e.target.parentNode.parentNode;
+    const readTextCard = e.target.parentNode.parentNode.firstElementChild.lastElementChild;
+
+    // find the index of the book and update the read variable
+    const filteredBook = books.filter(book => {
+      return book.id === +(selectedCard.dataset.number);
+    });
+    const bookIndex = books.indexOf(filteredBook[0]);
+    if (bookIndex != -1) {
+      books[bookIndex].read = (books[bookIndex].read) === "Not read" ? "Read" : "Not read";
+      readTextCard.innerHTML = books[bookIndex].read;
+    }
+    
   }
 });
